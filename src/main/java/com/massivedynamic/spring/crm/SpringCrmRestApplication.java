e35951baa6c3403e6bad23033d0794f94881107b
ppalwai <praveen.palwai@gmail.com>
2021-02-21 17:04:28 -0500

    Initial Commit

---
+++ src/main/java/com/massivedynamic/spring/crm/SpringCrmRestApplication.java
@@ -0,0 +1,13 @@
+package com.massivedynamic.spring.crm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCrmRestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringCrmRestApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/massivedynamic/spring/crm/SpringCrmRestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.massivedynamic.spring.crm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCrmRestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
